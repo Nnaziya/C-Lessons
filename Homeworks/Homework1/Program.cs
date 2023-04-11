4bf6777296c6075b1e80af941f065dd078228eff
Nazya <nazi.biw@gmail.com>
2023-04-11 23:53:15 +0300

    Homework1

---
+++ Homeworks/Homework1/Program.cs
@@ -0,0 +1,81 @@
+﻿
+// 4. На вход принимает два числа и выдает какое число больше, а какое меньше
+// a = 5, b = 7, max = 7;
+// a = 12, b = 4, max = 12
+
+// Console.WriteLine("Введите первое число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+// if (a > b)
+//  {
+//    Console.WriteLine($"max -> {a}"); 
+//  }
+// else
+//  {
+//    Console.WriteLine($"max -> {b}"); 
+//  }
+
+
+
+// 5. На вход принимает три числа и выдает макс из этих чисел
+// 2, 3, 7, max = 7
+// 45, 1, 12, max = 45
+
+// Console.WriteLine("Введите первое число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите третье число: ");
+// int c = Convert.ToInt32(Console.ReadLine());
+//  int max = 0;
+// if(a > max)
+// {
+//   max = a;
+// }
+// if(b > max)
+// {
+//   max = b;
+// }
+// if(c > max)
+// {
+//   max = c;
+// }
+// Console.WriteLine($"Максимальное число -> {max}"); 
+
+
+
+
+// 6. На вход принимает число и выдает является ли число четным (делится на два)
+// 4 = да
+// -3 = нет
+
+// Console.WriteLine("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// if (number % 2 == 0)
+// {
+//   Console.WriteLine($"{number} - Четное число"); 
+// }
+// else
+// {
+//   Console.WriteLine($"{number} - Нечетное число"); 
+// }
+
+
+
+
+// 7. На вход число (N), на выходе показывает все четные числа от 1 до N
+// 5 -> 2,4
+// 12 -> 2, 4, 6, 8, 10
+
+// Console.WriteLine("Введите число: ");
+// int x = Convert.ToInt32(Console.ReadLine());
+// int y = 2;
+// if(x > 1)
+// {
+//   while (y <= x)
+//     {
+//         Console.Write(y + " ");
+//         y = y + 2;
+//     }
+// }
